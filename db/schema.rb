35f45d198dd769238ef6d101c1aba422e4baf898
fedetaglia <fede.tagliabue@gmail.com>
2014-02-24 17:20:53 +1100

    fix models relationship between users - groups - expenses - portions (hope to .. )

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224040017) do
+ActiveRecord::Schema.define(version: 20140224061445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20140224040017) do
 
   create_table "portions", force: true do |t|
     t.decimal  "amount"
-    t.integer  "expenses_id"
+    t.integer  "expense_id"
     t.integer  "payee_id"
     t.datetime "created_at"
     t.datetime "updated_at"
