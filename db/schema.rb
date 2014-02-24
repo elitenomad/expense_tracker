5b3d333f62980b38136e90cce092eea922199e05
fedetaglia <fede.tagliabue@gmail.com>
2014-02-24 23:27:39 +1100

    decimal datatype for amount into portion table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224061445) do
+ActiveRecord::Schema.define(version: 20140224122446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20140224061445) do
   add_index "groups_users", ["user_id", "group_id"], name: "index_groups_users_on_user_id_and_group_id", using: :btree
 
   create_table "portions", force: true do |t|
-    t.decimal  "amount",     precision: 8, scale: 2
+    t.decimal  "amount"
     t.integer  "expense_id"
     t.integer  "payee_id"
     t.datetime "created_at"
