1122f8a346c634d2089e0b3b276ee631da16e899
elitenomad <stalin.pranava@gmail.com>
2014-02-24 12:17:17 +1100

    Initializing the Expense Tracker Repo for Team

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExpenseTracker::Application.config.secret_key_base = '6637dc8cc6495e4596c2fa31ae46f83c049f5e2719f6b0c2da2ec4771f71e449ea11433712285824f6d76db66b9d6fc09317b6f8ff507fcc65be4064958167ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExpenseTracker::Application.config.session_store :cookie_store, key: '_expense_tracker_session'
