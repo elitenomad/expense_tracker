ad365699b31260a64c3e44e0aab89911d23cf8b5
fedetaglia <fede.tagliabue@gmail.com>
2014-02-26 12:26:29 +1100

    modified expense , portion and group model
    add scope to expense and portion

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,10 @@ class Group < ActiveRecord::Base
   include AASM
   belongs_to :owner, class_name: 'User'
   has_and_belongs_to_many :users
+
   has_many :expenses
+
+  has_many :portions, through: :expenses
 
   validates :name, presence: true
 
--- app/models/portion.rb
+++ app/models/portion.rb
@@ -2,9 +2,12 @@ class Portion < ActiveRecord::Base
   include AASM
 
   belongs_to :expense
+  belongs_to :group
 
   belongs_to :payee, class_name: 'User'
 
+  scope :settled, { where settled: true }
+  scope :current, { where settled: false }
 
   # manage the settled value with AASM 
   aasm :column => 'settled' do
