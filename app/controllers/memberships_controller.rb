bd8f5cc22191f26a8b7b49ccbd9ce0f514a0ef89
fedetaglia <fede.tagliabue@gmail.com>
2014-02-26 16:29:21 +1100

    add regenerate portions when add new group memeber

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -13,6 +13,10 @@ def create
       user = User.find_by(email: params[:emailid])
       if (group_users_count == 0)
         @group.users << user
+        #when added a new user the portions have to be regenerated
+        @group.expenses.each do |expense|
+          expense.regenerate_portions
+        end
         redirect_to 'groups_path'
       else
         redirect_to groupuser_path(@group), alert: "User is already added to group"
