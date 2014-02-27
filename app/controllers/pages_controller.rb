72b27c0cd94a30726422a7250a573bca896a5b68
elitenomad <stalin.pranava@gmail.com>
2014-02-27 20:29:08 +1100

    Partials contact and footer changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,16 @@ skip_before_filter :authenticate_user!, only: [:index]
   def about
   end
 
+  def sendemail
+    begin
+      UserMailer.feedback_mail(params).deliver
+      UserMailer.contact_mail(params).deliver
+      redirect_to root_path
+    rescue
+      flash.now[:notice] = "Error in sending email. Please try again"
+      redirect_to root_path
+    end
+      
+  end
+
 end
