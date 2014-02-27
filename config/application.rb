50cc56a0b12ab534e2bae60aeda81934183d87b3
MollyMoile <marcus.hoile@gmail.com>
2014-02-27 23:55:24 +1100

    landing page draft, styling changes on header plus more described below uploaded font awesome, cant get to work sign up button color change

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
