0e4a78f485aca208913b05a61121eddd40739315
MollyMoile <marcus.hoile@gmail.com>
2014-02-28 01:31:14 +1100

    lots of styling little bits

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
